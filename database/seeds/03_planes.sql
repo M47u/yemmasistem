420f1edfffe8d459afc62ba36b41466782484816
Matias Aveiro <aveiromatias@gmail.com>
2026-07-06 10:45:14 -0300

    planes actualizados en el seed

--- database/seeds/03_planes.sql
+++ database/seeds/03_planes.sql
@@ -1,6 +1,4 @@
 INSERT IGNORE INTO planes (id, nombre, velocidad_mb, precio_base, activo) VALUES
-    (1, '10 Mbps',        10,  3000.00, 1),
-    (2, '20 Mbps',        20,  4500.00, 1),
-    (3, '30 Mbps',        30,  5500.00, 1),
-    (4, '50 Mbps',        50,  7000.00, 1),
-    (5, '100 Mbps Fibra', 100, 9500.00, 1);
+    (1, '10 Mbps',        10,  20000.00, 1),
+    (2, '15 Mbps',        20,  28000.00, 1),
+    (3, '20 Mbps',        30,  35000.00, 1);
